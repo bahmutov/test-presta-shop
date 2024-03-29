b6f3fe471f0193f06c654465b6cf6c929ffbd6f9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-29 15:51:21 -0400

    simple

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -3,31 +3,35 @@
 // https://github.com/bahmutov/cypress-network-idle
 import 'cypress-network-idle'
 
+const getIframeBody = (wrap = false) => {
+  if (wrap) {
+    return (
+      cy
+        .get('iframe[title="Frame of demo shop"]')
+        .its('0.contentDocument.body')
+        // we know the iframe body is there
+        // but we need a jQuery object to be able to use cy.contains
+        // otherwise we see "subject.each" is not a function error
+        .then(cy.wrap)
+    )
+  } else {
+    return cy
+      .get('iframe[title="Frame of demo shop"]')
+      .its('0.contentDocument.body')
+  }
+}
+
 it('goes to the checkout with one item', () => {
   cy.visit('/')
   cy.get('#loadingMessage', { timeout: 15_000 }).should('not.be.visible')
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
+  getIframeBody()
     .find('.header-top')
     .should('be.visible')
     .contains('a', 'Clothes')
     .click()
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
-    .find('.breadcrumb')
-    .should('include.text', 'Clothes')
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
-    // we know the iframe body is there
-    // but we need a jQuery object to be able to use cy.contains
-    // otherwise we see "subject.each" is not a function error
-    .then(cy.wrap)
-    .contains('.category-sub-menu a', 'Men')
-    .click()
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
-    .find('.breadcrumb')
-    .should('include.text', 'Men')
+  getIframeBody().find('.breadcrumb').should('include.text', 'Clothes')
+  getIframeBody(true).contains('.category-sub-menu a', 'Men').click()
+  getIframeBody().find('.breadcrumb').should('include.text', 'Men')
 
   cy.waitForNetworkIdlePrepare({
     method: 'GET',
@@ -36,16 +40,14 @@ it('goes to the checkout with one item', () => {
   })
 
   // click on the first product item
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
+  getIframeBody()
     .find('.products .product')
     .should('have.length.greaterThan', 0)
     .first()
     .click()
 
   // click on the "Add to cart" button
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
+  getIframeBody()
     .find('#main .product-container')
     .should('be.visible')
     .within(() => {
@@ -54,22 +56,19 @@ it('goes to the checkout with one item', () => {
       cy.contains('button', 'Add to cart').should('be.visible').click()
     })
 
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
+  getIframeBody()
     .find('#blockcart-modal')
     .should('be.visible')
     .contains('a', 'Proceed to checkout')
     .click()
 
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
+  getIframeBody()
     .find('.cart-grid')
     .should('be.visible')
     .contains('a', 'Proceed to checkout')
     .click()
 
-  cy.get('iframe[title="Frame of demo shop"]')
-    .its('0.contentDocument.body')
+  getIframeBody()
     .find('#checkout-personal-information-step')
     .should('be.visible')
 })
